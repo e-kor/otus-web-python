f2bb301b0aaaf17c6f0d7bb132b461fc31a50927
ek <turokg@yahoo.com>
2020-01-06 16:42:40 -0500

    minor-changes

--- task1_recur_search/main.py
+++ task1_recur_search/main.py
@@ -1,34 +1,6 @@
 import argparse
-import requests
-from bs4 import BeautifulSoup
 from itertools import islice
-from settings import API_KEY, BASE_URL, ENGINE_ID
-
-
-def search_web(*keywords):
-    request_url = f"{BASE_URL}?key={API_KEY}&cx={ENGINE_ID}&q={' '.join(keywords)}"
-    response = requests.get(request_url)
-    yield from map(lambda item: item.get('link'), response.json()['items'])
-
-
-def extract_nested_urls(url):
-    try:
-        page = requests.get(url).text
-    except requests.exceptions.ConnectionError:
-        return []
-    soup = BeautifulSoup(page, 'html.parser')
-    all_urls = map(lambda block: block.get('href'), soup.find_all('a'))
-    yield from filter(lambda link: link and link.startswith('http'), all_urls)
-
-
-def recursive_extract_urls(*urls, level=0):
-    for url in urls:
-        if level <= 0:
-            yield url
-        elif level == 1:
-            yield from extract_nested_urls(url)
-        else:
-            yield from recursive_extract_urls(*extract_nested_urls(url), level=level - 1)
+from utils import search_web, extract_nested_urls, recursive_extract_urls
 
 
 if __name__ == '__main__':
