30a78fe3e4a7826536540c1914988e08d28c004d
ek <turokg@yahoo.com>
2020-01-07 00:24:39 -0500

    loto

--- task2_loto/classes.py
+++ task2_loto/classes.py
@@ -14,26 +14,21 @@ class Card:
 
     def __hash__(self):
         res = 0
-
         for num in self.numbers:
             res += hash(num)
-
         return res
 
     def __eq__(self, other):
         if not isinstance(other, Card):
             raise TypeError
-
         for num in self.numbers:
             if num not in other.numbers:
                 return False
-
         return True
 
     def __repr__(self):
         symbols = [
             f'{"X " if num in self.checked_numbers else "  "}{num}' for num in self.numbers]
-
         return '|'.join(symbols)
 
 
@@ -60,25 +55,20 @@ class Master:
     def show_number(self) -> int:
         number = self.numbers_queue.pop()
         self.numbers_shown.add(number)
-
         return number
 
     def get_card(self) -> Card:
         numbers = set()
-
         while len(numbers) < NUMBERS_IN_CARD_COUNT:
             numbers.add(choice(self.numbers_queue))
         card = Card(numbers)
-
         if card in self.cards_given:
             card = self.get_card()
         self.cards_given.add(card)
-
         return card
 
     def is_checked_right(self, card: Card) -> bool:
         for num in card.checked_numbers:
             if num not in card.numbers or num not in self.numbers_shown:
                 return False
-
             return True
--- task2_loto/main.py
+++ task2_loto/main.py
@@ -5,9 +5,7 @@ def run_game():
     players_count = int(input('How many players will play?\n'))
     master = Master()
     players = [Player(i, master.get_card()) for i in range(players_count)]
-
     n = 0
-
     while True:
         n += 1
         number = master.show_number()
@@ -16,11 +14,9 @@ def run_game():
         winners = list(filter(lambda p: p.has_won, players))
         true_winners = list(
             filter(lambda w: master.is_checked_right(w.card), winners))
-
         if true_winners:
             print(f"We've got a winner:")
             print('\n'.join([repr(p) for p in winners]))
-
             break
         print('\n'.join([repr(p) for p in players]))
         print()
