5599511983073469cbb879853cf2c998295aef1e
Egor Korovin <ek@EK-MacBook-Pro.local>
2020-01-13 16:44:30 -0500

    renaming

--- task2_loto/loto/settings.py
+++
@@ -1,6 +0,0 @@
-# some constants of cards
-NUMBERS_IN_CARD_COUNT = 5
-ROWS_COUNT = 3
-COLS_COUNT = 9
-BLANK_SYMBOL = '__'
-CHECKED_SYMBOL = 'XX'
--- task2_loto/setup.py
+++
@@ -1,3 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(name="loto", packages=find_packages())
