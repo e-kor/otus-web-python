d2e8190354694cb38e98f90969aa16fd14c8518e
ek <turokg@icloud.com>
2020-01-25 22:52:01 +0700

    added models

--- 5_django/coursera/courses/models.py
+++ 5_django/coursera/courses/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+
+class Tutor(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+
+class Student(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+
+class Course(models.Model):
+    tutor = models.ForeignKey(Tutor, on_delete=models.SET_NULL, null=True)
+    students = models.ManyToManyField(Student)
+
+
+class Lesson(models.Model):
+    date = models.DateTimeField()
+    title = models.CharField(max_length=100)
+    synopsis = models.CharField(max_length=1000, blank=True, null=True)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
