98c90c8ab66e2f10e9560d8303b5eeee2b2e5e5c
ek <turokg@icloud.com>
2020-05-05 19:46:46 +0700

    improved tags display added blank files for course details

--- 8_react/src/components/CourseListItem.jsx
+++ 8_react/src/components/CourseListItem.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import './CourseListItem.css'
-import InfiniteScroll from "react-infinite-scroll-component";
 import Tag from "./Tag";
 
 const CourseListItem = props => {
--- 8_react/src/components/Tag.jsx
+++ 8_react/src/components/Tag.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
+import './Tag.css'
 
 const Tag = props => {
     const {name} = props;
 
     return (
         <small className="tag">#{name} </small>
-
     )
-}
+};
 
 export default Tag;
